87a077e4985cbf53d6590363fdaa5b4f6f465a75
exima09 <dawidjuszczak01@gmail.com>
2018-12-22 12:04:04 +0100

    add catalogs

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,10 @@ import './App.css';
 
 import logo from './logo.svg';
 
-const test = [1, 2,
-  3, 4, 5,
-        6, 7, 8
-];
-
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        {console.log(test,          "fds fsdf dsff")}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
