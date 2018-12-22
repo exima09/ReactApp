90b693933791614908a6031de0b4532a5a196ed6
exima09 <dawidjuszczak01@gmail.com>
2018-12-22 11:54:49 +0100

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 
 import logo from './logo.svg';
 
-const test = [1,2,
-3, 4, 5,
+const test = [1, 2,
+  3, 4, 5,
   6, 7, 8
 ];
 
@@ -12,9 +12,10 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        {console.log(test)}
+        {console.log(test
+        )}
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
